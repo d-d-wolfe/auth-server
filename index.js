ecfaff38675f2f63e4825078b60c4a4c9de0d374
d-d-wolfe <dave.wolfe623@gmail.com>
2020-06-22 15:54:00 -0700

    made a few additions. testing the new upstream

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
 
 require('dotenv').config();
+const server = require('./src/server.js');
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const bcrypt = require('bcrypt');
 const cors = require('cors');
 const base64 = require('base-64');
 const jwt = require('jsonwebtoken');
 
+
 const app = express();
 
 app.use(express.json());
 
+server.start(PORT);
